a03e2f9fc282742045b7dc37fb2b44a3a25e29c8
jaanvibaratam <101267617+jaanvibaratam@users.noreply.github.com>
2022-10-10 12:09:25 +0530

    Create nestingofmemberfunc.cpp

---
+++ nestingofmemberfunc.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+class binary
+{
+	private:
+		    string s;
+	public:
+		   void read(void)
+		   {
+		   	cout<<"enter a binary number:";
+		   	cin>>s;
+		   }
+		   void chk_bin(void)
+		   {
+		   	for(int i=0;i<s.length();i++)
+		   	{
+		   		if(s.at(i)!='0' && s.at(i)!='1')
+		   		{
+		   			cout<<"\n incorrect binary number format...the program will quit";
+		   			getch();
+		   			exit(0);
+				}
+			}
+		   }
+		   void ones(void)
+		   {
+		   	chk_bin();
+		   	for(int i=0;i<s.length();i++)
+		   	{
+		   		if(s.at(i)=='0')
+		   		   s.at(i)='1';
+		   		else
+		   		   s.at(i)='0';
+			   }
+		   }
+		   void displayones(void)
+		   {
+		   	ones();
+		   	cout<<"\n the 1's compliment of the above binary number is:"<<s;
+		   }
+};
+int main()
+{
+	binary b;
+	b.read();
+	b.displayones();
+	getch();
+	return 0;
+}
